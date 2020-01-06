fbb1043ea2d8b9df595aace1ed280ae5e7b5c779
Zim Kalinowski <zikalino@microsoft.com>
2020-01-06 09:22:49 +0800

    added first namer

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { AutoRestExtension, Channel, Host, startSession } from '@azure-tools/autorest-extension-base';
 import { codeModelSchema, CodeModel } from '@azure-tools/codemodel';
 import { serialize } from '@azure-tools/codegen';
+import { Namer } from './namer';
 
 export type LogCallback = (message: string) => void;
 export type FileCallback = (path: string, rows: string[]) => void;
@@ -12,19 +13,10 @@ extension.Add("cli.common", async autoRestApi => {
 
     try
     {
-        // read files offered to this plugin
-        const inputFileUris = await autoRestApi.ListInputs();
-
-        const inputFiles: string[] = await Promise.all(inputFileUris.map(uri => autoRestApi.ReadFile(uri)));
-
-        // read a setting
-
         const isDebugFlagSet = await autoRestApi.GetValue("debug");
         let cliCommonSettings = await autoRestApi.GetValue("cli");
 
         const session = await startSession<CodeModel>(autoRestApi, {}, codeModelSchema);
-
-        // emit messages
 
         autoRestApi.Message({
             Channel: Channel.Warning,
@@ -36,15 +28,14 @@ extension.Add("cli.common", async autoRestApi => {
             Text: "cli.common settings " + JSON.stringify(cliCommonSettings)
         });
 
-        autoRestApi.Message({
-            Channel: Channel.Information,
-            Text: "AutoRest offers the following input files: " + inputFileUris.join(", "),
-        });
 
+        const plugin = await new Namer(session).init();
+        const result = plugin.process();
 
-        let result = session.model;
-
-        result["ADDITIONAL"] = "TEST";
+        // add test scenario from common settings
+        if (cliCommonSettings) {
+            result["test-scenario"] = cliCommonSettings['test-scenario'];
+        }
 
         // emit a file (all input files concatenated)
         autoRestApi.WriteFile("code-model-v4-cli.yaml", serialize(result, codeModelSchema));
