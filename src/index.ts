c72c0bbb1a8d49a32f635622e47af9040db18469
Zim Kalinowski <zikalino@microsoft.com>
2020-01-23 09:50:11 +0800

    removed some unnecessary output

--- src/index.ts
+++ src/index.ts
@@ -24,50 +24,18 @@ extension.Add("clicommon", async autoRestApi => {
         const namer = await new Namer(session).init();
         let result = namer.process();
 
-        autoRestApi.Message({
-            Channel: Channel.Warning,
-            Text: "XXX --- namer finished"
-        });
-
         const modifiers = new Modifiers(session);
-
-        autoRestApi.Message({
-            Channel: Channel.Warning,
-            Text: "XXX --- created modifiers"
-        });
-
         modifiers.codeModel = result;
         modifiers.directives = (cliCommonSettings != null) ? cliCommonSettings['directives'] : null;
-
-        autoRestApi.Message({
-            Channel: Channel.Warning,
-            Text: "XXX --- processing modifiers"
-        });
-
         result = await modifiers.process();
-
-        autoRestApi.Message({
-            Channel: Channel.Warning,
-            Text: "XXX --- modifiers-processed"
-        });
 
         // add test scenario from common settings
         if (cliCommonSettings) {
             result["test-scenario"] = cliCommonSettings['test-scenario'];
         }
 
-        autoRestApi.Message({
-            Channel: Channel.Warning,
-            Text: "XXX --- writing file"
-        });
         // emit a file (all input files concatenated)
         autoRestApi.WriteFile("code-model-v4-cli.yaml", serialize(result));
-
-        autoRestApi.Message({
-            Channel: Channel.Warning,
-            Text: "XXX --- file written"
-        });
-
     }
     catch (e)
     {
