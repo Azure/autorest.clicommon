0e68abf835c38c89b426e02c6c9044c5a5b4b4cb
Changlong Liu <changll@microsoft.com>
2020-12-17 09:49:44 +0800

    check null

--- src/index.ts
+++ src/index.ts
@@ -25,8 +25,8 @@ extension.Add("cli-test", async host => {
     // add test scenario from common settings
     const testCommonSettings = await host.GetValue("test");
     const cliCommonSettings = await host.GetValue("cli");
-    if (testCommonSettings || cliCommonSettings) {
-        session.model["test-scenario"] = testCommonSettings['test-scenario'] || testCommonSettings['test-setup'] || cliCommonSettings['test-scenario'] || cliCommonSettings['test-setup'];
+    if (testCommonSettings) {
+        session.model["test-scenario"] = testCommonSettings?.['test-scenario'] || testCommonSettings?.['test-setup'] || cliCommonSettings?.['test-scenario'] || cliCommonSettings?.['test-setup'];
     }
 
     dumper.dumpCodeModel("test-post", session.model);
