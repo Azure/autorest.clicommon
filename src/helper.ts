0cbd56d329c89b81c348c49353b8791532e442e9
FAREAST\renhel <renhel@microsoft.com>
2021-01-18 12:37:34 +0800

    Fix lint issue and add eslint into compile

--- src/helper.ts
+++ src/helper.ts
@@ -644,7 +644,7 @@ export class Helper {
         if (!enumExample || isNullOrUndefined(op.extensions?.['x-ms-examples'])) return;
         const cliKeyMissing = '<clikey-missing>';
             
-        for (let exampleName of Object.getOwnPropertyNames(op.extensions['x-ms-examples'])) {
+        for (const exampleName of Object.getOwnPropertyNames(op.extensions['x-ms-examples'])) {
             const example = op.extensions['x-ms-examples'][exampleName];
             paths.push(`['${exampleName}']`);
             action({
@@ -917,20 +917,20 @@ export class Helper {
         return m4Path;
     }
 
-    public static setPathValue (obj, path, value)  {
+    public static setPathValue(obj, path, value)  {
         if (Object(obj) !== obj) return obj; // When obj is not an object
         // If not yet an array, get the keys from the string-path
         if (!Array.isArray(path)) path = path.toString().match(/[^.[\]]+/g) || []; 
         path.slice(0,-1).reduce((a, c, i) => // Iterate all of them except the last one
-             Object(a[c]) === a[c] // Does the key exist and is its value an object?
-                 // Yes: then follow that path
-                 ? a[c] 
-                 // No: create the key. Is the next key a potential array-index?
-                 : a[c] = Math.abs(path[i+1])>>0 === +path[i+1] 
-                       ? [] // Yes: assign a new array object
-                       : {}, // No: assign a new plain object
-             obj)[path[path.length-1]] = value; // Finally assign the value to the last key
+            Object(a[c]) === a[c] // Does the key exist and is its value an object?
+            // Yes: then follow that path
+                ? a[c] 
+            // No: create the key. Is the next key a potential array-index?
+                : a[c] = Math.abs(path[i+1])>>0 === +path[i+1] 
+                    ? [] // Yes: assign a new array object
+                    : {}, // No: assign a new plain object
+        obj)[path[path.length-1]] = value; // Finally assign the value to the last key
         return obj; // Return the top-level object to allow chaining
-    };
+    }
 
 }
